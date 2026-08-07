32b5a4d772388813ab883fb7017302023089d514
anushan23 <anushan23@gmail.com>
2026-08-07 12:35:11 -0500

    Creating lfeaturefile,step file and page object for login page

---
+++ features/login.feature
@@ -0,0 +1,122 @@
+@login
+Feature: login page
+
+@validURL
+Scenario: Login page is displayed successfully
+    Given Admin is on the browser
+    When Admin enters the Valid LMS app URL
+    Then Admin should land on the login page
+
+@InvalidURL
+Scenario: Access app with invalid URL 
+      Given Admin is on the browser
+      When Admin enters the invalid LMS app URL.
+      Then Admin should receive error message
+      
+@ApplicationLogo
+Scenario: Application Logo is displayed
+   Given Admin is on the browser
+   When Admin enters the Valid LMS app URL 
+   Then Admin should see Application Logo
+  
+@LoginInstructionMessage   
+Scenario:Login instruction message is displayed
+   Given Admin is on the browser
+   When Admin enters the Valid LMS app URL 
+   Then Admin should see "Please login to LMS application"
+
+@InputFieldsDisplayed
+Scenario: Input fields are displayed
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see two text field
+
+@RoleDropdown
+Scenario: Role dropdown is displayed
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see one dropdown
+
+@UserTextPresence
+Scenario:Text presence on the first field
+     Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see "User" in the first text field
+
+@PasswordTextPresence
+Scenario:Text presence on the second field
+     Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see "Password" in the second text field
+
+@AsteriskUserPresence
+Scenario:Asterisk is displayed for user field
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see asterisk mark symbol next to user text
+
+@AsteriskPasswordPresence
+Scenario:Asterisk is displayed for password field
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see asterisk mark symbol next to password text
+
+@PlaceholderPresence      
+Scenario:Placeholder presence in dropdown 
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see "Select the role" placeholder in dropdown
+
+@DropdownOptions
+Scenario:Dropdown options to select role
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see "Admin", " Staff " ," Student " options in dropdown
+
+@LoginFormPresence
+Scenario:Alignment of the login form
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see login form on the centre of the page
+
+@LoginButtonDisplayed
+Scenario:Login button is displayed
+      Given Admin is on the browser
+      When Admin enters the Valid LMS app URL
+      Then Admin should see login button 
+
+@UserTextColour
+Scenario:User field Placeholder text colour
+    Given Admin is on the browser
+    When Admin enters the Valid LMS app URL
+    Then Admin should see user text in gray color
+
+@passwordTextColour
+Scenario:Password field Placeholder text colour
+    Given Admin is on the browser
+    When Admin enters the Valid LMS app URL
+    Then Admin should see password text in gray color
+
+@SuccessfulLogin
+Scenario:Successful login with valid credentials
+    Given Admin is on login Page
+    When Admin clicks login in button after entering  a valid credential
+    Then Admin should land on home page 
+
+@splcharacLogin
+Scenario: Login with spl charac in user name
+    Given Admin is on login Page
+    When Admin clicks login in button after entering special character in username
+    Then Admin should see Error message "Inactive User : Please contact Admin for assistance"
+
+@EmptyUserNameLogin
+ Scenario: Login attempt with empty username
+    Given Admin is on login Page
+    When Admin has entered only the password and selected a role
+    Then Admin should see Error message "Please enter your user name"
+
+@EmptyPasswordLogin
+ Scenario: Login attempt with empty password
+    Given Admin is on login Page
+    When Admin has entered only the username and selected a role
+    Then Admin should see Error message "Please enter your password " 
---
+++ features/step_definitions/login.step.js
@@ -0,0 +1,158 @@
+const {Given, When, Then ,setDefaultTimeout } = require('@cucumber/cucumber');
+const assert = require('assert');
+const datajson = JSON.parse(JSON.stringify(require('../../utils/loginTestData.json')));
+const { expect } = require('@playwright/test');
+
+setDefaultTimeout(60 * 1000);
+
+//@validURL
+Given('Admin is on the browser', async function () {
+ console.log('Admin is on the browser');
+});
+
+When('Admin enters the Valid LMS app URL', async function () {
+    await this.loginPage.navigateToLoginPage(this.baseUrl);
+});
+
+Then('Admin should land on the login page', async function () {
+    const isVisible = await this.loginPage.isLoginButtonVisible();
+    assert.equal(isVisible, true);
+    console.log('Admin is on the login page');
+});
+
+//@InvalidURL
+When('Admin enters the invalid LMS app URL.', async function () {
+    await this.loginPage.navigateToInvalidURL(datajson.invalidUrl);
+});
+
+Then('Admin should receive error message', async function () {
+    const errorMessage = await this.loginPage.invalidPageDisplayed();
+    assert.equal(errorMessage, true);
+});
+
+
+
+
+/*Then('HTTP response should be greater than or equal to 400. Then the link is broken', async function () {
+    const isBroken = await this.loginPage.verifyHTTPResponse(400);
+    expect(isBroken).toBeTruthy();
+
+});*/
+
+//@ApplicationLogo
+Then('Admin should see Application Logo', async function () {
+         const isLogoVisible = await this.loginPage.verifyCompanyLogo();
+         assert.equal(isLogoVisible, true);
+});
+
+//@LoginInstructionMessage
+Then('Admin should see {string}', async function (string) {
+    await expect(this.loginPage.loginInstruction).toHaveText(string);
+});
+
+//@InputFieldsDisplayed
+Then('Admin should see two text field', async function () {
+   await expect(this.loginPage.username).toBeVisible();
+   await expect(this.loginPage.password).toBeVisible();
+});
+
+//@RoleDropdown
+Then('Admin should see one dropdown', async function () {
+    await expect(this.loginPage.roleDropdown).toBeVisible();
+});
+
+//@UserTextPresence
+Then('Admin should see {string} in the first text field', async function (expectedText) {
+   await expect(this.loginPage.userLabel).toHaveText(expectedText);
+});
+
+//@PasswordTextPresence
+Then('Admin should see {string} in the second text field', async function (expectedText) {
+     const placeholder = await this.loginPage.password.getAttribute('data-placeholder');
+    assert.equal(placeholder, expectedText);
+});
+
+
+//@AsteriskUserPresence
+Then('Admin should see asterisk mark symbol next to user text', async function () {
+   await expect(this.loginPage.userAsterisk).toHaveText('*');
+   await expect(this.loginPage.userAsterisk).toBeVisible();
+});
+
+//@AsteriskPasswordPresence
+Then('Admin should see asterisk mark symbol next to password text', async function () {
+    await expect(this.loginPage.passwordAsterisk).toHaveText('*');
+    await expect(this.loginPage.passwordAsterisk).toBeVisible();
+});
+
+//@PlaceholderPresence 
+Then('Admin should see {string} placeholder in dropdown', async function (expectedText) {
+       await expect(this.loginPage.rolePlaceholder).toHaveText(expectedText);
+});
+
+//@DropdownOptions
+Then('Admin should see {string}, {string} ,{string} options in dropdown', async function (string, string2, string3) {
+  await this.loginPage.roleDropdown.click();
+  await expect(this.loginPage.roleSelect).toHaveText(string);
+  await expect(this.loginPage.staffOption).toHaveText(string2);
+  await expect(this.loginPage.studentOption).toHaveText(string3);
+});
+
+//@LoginFormPresence
+Then('Admin should see login form on the centre of the page', async function () {
+console.log(await this.loginPage.loginForm.count());
+});
+
+//@LoginButtonDisplayed
+Then('Admin should see login button', async function () {    
+const isLoginButtonVisible = await this.loginPage.isLoginButtonVisible();
+assert.equal(isLoginButtonVisible, true);
+});
+
+
+//@UserTextColour
+Then('Admin should see user text in gray color', async function () {
+
+    const color = await this.loginPage.userLabel.evaluate(
+        (element) => window.getComputedStyle(element).color
+    );
+    console.log("User placeholder color:", color);
+    await expect(color).toBe('rgba(0, 0, 0, 0.54)'); 
+});
+
+//@passwordTextColour
+Then('Admin should see password text in gray color', async function () {
+
+    const color = await this.loginPage.passwordLabel.evaluate(
+        (element) => window.getComputedStyle(element).color
+    );
+    console.log("Password placeholder color:", color);
+    await expect(color).toBe('rgba(0, 0, 0, 0.54)');
+});
+
+//@SuccessfulLogin
+Given('Admin is on login Page', async function () {
+ await this.loginPage.navigateToLoginPage(this.baseUrl);
+});
+
+When('Admin clicks login in button after entering  a valid credential', async function () {
+  await this.loginPage.loginWithCredentials(datajson.username, datajson.password);
+  await this.loginPage.clickLoginButton();
+});
+
+Then('Admin should land on home page', async function () {
+  assert.equal(await this.loginPage.getpagetitle(), 'LMS');
+});
+
+
+
+
+
+
+
+
+
+
+
+  
+
