403c63a5355f64db9e49ec74a5d06f22006c5dbe
anushan23 <anushan23@gmail.com>
2026-08-11 01:03:13 -0500

    Added new scenario for checking input field alignment

--- pageobjects/loginPage.js
+++ pageobjects/loginPage.js
@@ -2,7 +2,11 @@ class loginPage {
     constructor(page) {
         this.page = page;
         this.username = page.getByRole('textbox', { name: 'User' });
+        this.usernameInput = page.locator('#username');
         this.password = page.getByRole('textbox', { name: 'Password' });
+        this.pwdLabel = page.getByText('Password', { exact: true });
+        this.passwordInput = page.locator('#password');
+        this.roleInput = this.page.locator( 'mat-select[formcontrolname="roleSelected"]' );
         this.passwordlocator =  page.locator('div').filter({ hasText: 'Password *' }).nth(3)
         this.roleDropdown = page.getByText('Select the roleSelect the');
         this.rolePlaceholder = page.getByLabel('Select the role').getByText('Select the role');
@@ -15,7 +19,6 @@ class loginPage {
         this.passwordAsterisk = this.page.locator('.mat-form-field-required-marker').nth(1);
         this.staffOption = page.getByRole('option', { name: 'Staff' });
         this.studentOption = page.getByRole('option', { name: 'Student' });
-        this.loginForm = page.locator('mat-card');
         this.loginInstruction = page.getByText('Please login to LMS');
         this.passwordLabel = this.page.locator('label[for="password"]');
         this.inactiveUserMessage = page.getByText('Inactive User : Please contact your administrator.');
